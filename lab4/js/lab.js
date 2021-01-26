02067df4f9b23443988f0042ee0540f74329a159
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-01-26 14:21:01 -0800

    stuff for index

---
+++ lab4/js/lab.js
@@ -0,0 +1,47 @@
+// Loop three times
+    // Ask the user to input rock, paper, or scissors
+      // Validate input
+   // The computer selects random choice
+  // Compare choices
+      // If the player selects rock
+          // If the computer selects rock
+              // Declare tie
+         // If the computer selects paper
+             // Declare loss
+        // If the computer selects scissors
+            // Declare win
+     // If the player selects paper
+       // If the computer selects rock
+           // Declare win
+       // If the computer selects paper
+          // Declare tie
+      // If the computer selects scissors
+         // Declare loss
+    // If the player selects scissors
+      // If the computer selects rock
+        // Declare loss
+     // If the computer selects paper
+       // Declare win
+    // If the computer selects scissors
+      // Declare tie
+// Count the number of Player wins
+  // If the player has more wins than the computer, declare the player the winner
+  // If the computer has more wins, tell the player they lost
+
+  // Remove bike tire
+    // Disengage the bike break
+    // Unscrew wheel
+  // Remove the innertube from the tire
+    // Bike lever to pry off the tire from the innertube
+  // Fix innertube
+    // Inflate innertube
+    // Submerge innertube in water
+    // Look for bubbles and locate the leak
+    // Patch innertube
+  // Put innertube back in
+    // Line up the innertube valve with the valve hole in the tire
+  // Pump up innertube
+  // Put the tire back on the bike
+    // Make sure the tire is facing the right direction
+    // Screw tire in place
+    // Put break back in place
