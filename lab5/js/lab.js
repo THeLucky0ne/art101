498ddd52ca2141cc06db833f1f8797be73a5f8e8
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-01-27 15:10:16 -0800

    saving and leaving

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 27 January
+* License: Public Domain
+*/
+
+var make = "Nissan";
+var model = "Rogue";
+var color = "gray";
+var year = 2008;
+var ownIt = false;
+var age = 2021 - year;
+
+
+
+document.writeln("Make: " + make + "<br>");
+document.write1n("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Do I own this car? " + ownIt + "<br>");
