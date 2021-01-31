950a93fac78237b8ab4c84210962f0c1bf46fc06
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-01-30 16:53:57 -0800

    updated js file

---
+++ lab6/js/lab.js
@@ -0,0 +1,18 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 2 Febuary
+* License: Public Domain
+*/
+
+
+var myTransport = ["car", "walk", "bus"];
+var myMainRide = {
+  myMainRide.make = "Nissan",
+  myMainRide.model = "Rogue",
+  myMainRide.color = "gray",
+  myMainRide.year = "2008";
+}
+
+document.writeln("The transportation that I normally use: ", myTransport, "</br>");
+document.writeln("My main ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
