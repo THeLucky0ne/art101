b9a4f563de6eeceab98cea1b60db3279350dc027
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-01-30 17:10:40 -0800

    "added" stuff

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -13,6 +13,6 @@ var myMainRide = {
   myMainRide.year = "2008";
 }
 
-document.writeln("The transportation that I normally use: ", myTransport, "</br>");
+document.writeln("The transportation that I normally use: "+ myTransport + "</br>");
 document.writeln("My main ride: <pre>",
     JSON.stringify(myMainRide, null, '\t'), "</pre>");
