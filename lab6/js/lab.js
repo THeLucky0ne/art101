a634f3280e09c978cf598b2503d86813b20a32b7
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-01-30 17:13:28 -0800

    changes were made

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -7,12 +7,12 @@
 
 var myTransport = ["car", "walk", "bus"];
 var myMainRide = {
-  myMainRide.make = "Nissan",
-  myMainRide.model = "Rogue",
-  myMainRide.color = "gray",
-  myMainRide.year = "2008";
+  make = "Nissan",
+  model = "Rogue",
+  color = "gray",
+  year = "2008";
 }
 
-document.writeln("The transportation that I normally use: "+ myTransport + "</br>");
+document.writeln("The transportation that I normally use: ", myTransport, "</br>");
 document.writeln("My main ride: <pre>",
     JSON.stringify(myMainRide, null, '\t'), "</pre>");
