031305f9a4b8ada4887aacb153b327f67b3e8f23
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-08 17:04:54 -0800

    javascript stuff

---
+++ lab8/js/lab.js
@@ -0,0 +1,50 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 9 February
+* License: Public Domain
+*/
+
+
+function firstThing(test) {
+  console.log(test + "This is the first function");
+}
+function secondThing(test) {
+  console.log(test + "This is the second function");
+}
+function thirdThing(test) {
+  console.log(test + "This is the third function");
+}
+
+// first test
+
+firstThing("TEST 1");
+secondThing("TEST 1");
+thirdThing("TEST 1");
+
+// second test
+
+setTimeout(function() {
+  firstThing("TEST 2");
+}, 0)
+
+setTimeout(function() {
+  secondThing("TEST 2");
+}, 0)
+
+setTimeout(function() {
+  thirdThing("TEST 2");
+}, 0)
+
+// third test
+
+setTimeout(function() {
+  firstThing("TEST 2");
+}, 5000)
+
+setTimeout(function() {
+  secondThing("TEST 2");
+}, 3000)
+
+setTimeout(function() {
+  thirdThing("TEST 2");
+}, 4000)
