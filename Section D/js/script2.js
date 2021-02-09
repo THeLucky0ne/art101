37e4c5196a9571ae2e102a3fef2bd6dc27205d51
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-09 14:34:44 -0800

    more labs

--- Section D/js/script2.js
+++ Section D/js/script2.js
@@ -3,7 +3,7 @@ divEl.innerHTML="This is annoying";
 console.log(divEl);
 //console.log(divEl.innerHTML);
 var button = document.createElement("button");
-divEl.appendChild(button);
+document.body.appendChild(button);
 button.innerHTML = "Plz Press";
 
 button.onclick=function() {
