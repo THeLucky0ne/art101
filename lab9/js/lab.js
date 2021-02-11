08e7564f405d9aaefb96c96a263dcb65da662478
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-11 00:53:57 -0800

    updated code once more

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -11,6 +11,5 @@ var new1El = document.createElement("p");
 new1El.innerHTML = "I really love this class.";
 var new2El = document.createElement("b");
 new2El.innerHTML = "I actually hate this class.";
-
 document.body.appendChild(new1El);
 document.body.appendChild(new2El);
