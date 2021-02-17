0b64ff74c36e906513ee9384f8f49bc0e370552d
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-17 10:48:19 -0800

    changes were made

---
+++ lab11/js/lab.js
@@ -0,0 +1,14 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 18 February
+* License: Public Domain
+*/
+
+
+var challengeButton = $(#challenge);
+var problemsButton = $(#problems);
+var resultsButton = $(#results);
+
+$(challengeButton).append("<button>Click to show the challenge</button>");
+$(problemsButton).append("<button>Click to show the problems</button>");
+$(resultsButton).append("<button>Click to show the results</button>");
