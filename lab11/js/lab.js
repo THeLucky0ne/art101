27c814d6d713d163256a34255a60c2e5892c1001
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-17 11:22:20 -0800

    added quotations

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -18,17 +18,17 @@ $(challengeButton).click(function(){
   $("#challenge").text("The purpose of this lab is
   to experiment with jQuery by using buttons to
   display our paragraphs.")
-})
+});
 
 $(problemsButton).click(function(){
   $("problems").text("One issue that I experienced during
   this lab was gettig adjusted with the syntax for jQuery.
   With the help of WesSchools, I was able to figure out
   the syntax appropriate for this lab.")
-})
+});
 
 $(resultsButton).click(function(){
   $("results").text("There should be three buttons under each
   section. Clicking them should display their respective
   paragraphs.")
-})
+});
