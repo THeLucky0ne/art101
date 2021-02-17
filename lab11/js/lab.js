bf4c396f35f38a2466f81242339f686cab97068c
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-17 11:41:58 -0800

    changes in js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -15,13 +15,19 @@ $(resultsButton).append("<button>Click to show the results</button>");
 
 
 $(challengeButton).click(function(){
-  $("#challenge").
+  $("#challenge").append("<p>The purpose of this lab is to experiment with jQuery
+    by using buttons to display our paragraphs. </p>");
 });
 
 $(problemsButton).click(function(){
-  $("problems").
+  $("#problems").append("<p> One issue that I experienced during this lab was
+  gettig adjusted with the specific syntax jQuery has.
+  With Weschool's help, I was able to figure out
+  the syntax that would help me in this lab. </p>")
 });
 
 $(resultsButton).click(function(){
-  $("results").
+  $("#results").append("<p>There should be three buttons under each section.
+    Clicking on them should display their respective
+    paragraphs.</p>")
 });
