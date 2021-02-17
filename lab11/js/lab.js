f7c69cb65730b48033329d528ffd2ab79271f76b
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-17 11:20:57 -0800

    added descriptions for all

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -12,3 +12,23 @@ var resultsButton = $("#results");
 $(challengeButton).append("<button>Click to show the challenge</button>");
 $(problemsButton).append("<button>Click to show the problems</button>");
 $(resultsButton).append("<button>Click to show the results</button>");
+
+
+$(challengeButton).click(function(){
+  $("#challenge").text("The purpose of this lab is
+  to experiment with jQuery by using buttons to
+  display our paragraphs.")
+})
+
+$(problemsButton).click(function(){
+  $("problems").text("One issue that I experienced during
+  this lab was gettig adjusted with the syntax for jQuery.
+  With the help of WesSchools, I was able to figure out
+  the syntax appropriate for this lab.")
+})
+
+$(resultsButton).click(function(){
+  $("results").text("There should be three buttons under each
+  section. Clicking them should display their respective
+  paragraphs.")
+})
