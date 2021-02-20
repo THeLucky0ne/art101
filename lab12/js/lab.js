9abe8238f67afffa02b5277361582015433f2c3d
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-20 13:47:07 -0800

    changes were made

---
+++ lab12/js/lab.js
@@ -0,0 +1,34 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 23 February
+* License: Public Domain
+*/
+
+
+
+function sortingHat(str){
+  var length = str.length;
+  var mod = length % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+
+//str = document.getElementById("input")
+var theButton = document.getElementById("button")
+theButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  document.getElementById("output").innerHTML = "<p>The Sorting Hat has
+  sorted you into " + house + </p>
+})
