9f7bb59a98f0695fcdc4a4a4927148cab4bf34bc
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-24 10:40:28 -0800

    changed code and html

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -6,30 +6,31 @@
 
 
 
-
-for (i = 1; i <= 200; i ++) {
-  if (i % 105 == 0) {
-    console.log("FizzBuzzBoom")
-  }
-  else if (i % 15 == 0) {
-    console.log("FizzBuzz")
-  }
-  else if (i % 21 == 0) {
-    console.log("FizzBoom")
-  }
-  else if (i % 35 == 0) {
-    console.log("BuzzBoom")
-  }
-  else if (i % 3 == 0) {
-    console.log("Fizz")
-  }
-  else if (i % 5 == 0) {
-    console.log("Buzz")
-  }
-  else if (i % 7 == 0) {
-    console.log("Boom")
-  }
-  else {
-    console.log(i)
+function FuzzyBuzzy () {
+  for (i = 1; i <= 200; i ++) {
+    if (i % 105 == 0) {
+      console.log("FizzBuzzBoom")
+    }
+    else if (i % 15 == 0) {
+      console.log("FizzBuzz")
+    }
+    else if (i % 21 == 0) {
+      console.log("FizzBoom")
+    }
+    else if (i % 35 == 0) {
+      console.log("BuzzBoom")
+    }
+    else if (i % 3 == 0) {
+      console.log("Fizz")
+    }
+    else if (i % 5 == 0) {
+      console.log("Buzz")
+    }
+    else if (i % 7 == 0) {
+      console.log("Boom")
+    }
+    else {
+      console.log(i)
+    }
   }
 }
