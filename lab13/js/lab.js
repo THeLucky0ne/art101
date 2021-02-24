7e5bc8c9be877be7ad747bf4f1bd000d4482c0a0
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-02-24 10:31:28 -0800

    js updated for lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,35 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 25 February
+* License: Public Domain
+*/
+
+
+
+
+for (i = 1; i <= 200, i ++ 1) {
+  if (i % 105 == 0) {
+    console.log("FizzBuzzBoom")
+  }
+  else if (i % 15 == 0) {
+    console.log("FizzBuzz")
+  }
+  else if (i % 21 == 0) {
+    console.log("FizzBoom")
+  }
+  else if (i % 35 == 0) {
+    console.log("BuzzBoom")
+  }
+  else if (i % 3 == 0) {
+    console.log("Fizz")
+  }
+  else if (i % 5 == 0) {
+    console.log("Buzz")
+  }
+  else if (i % 7 == 0) {
+    console.log("Boom")
+  }
+  else {
+    console.log(i)
+  }
+}
