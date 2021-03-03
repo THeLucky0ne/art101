f1c5fc419afd182d5d9f99e7af585a6ea5af9aea
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-03-03 14:45:16 -0800

    javascript stuff

---
+++ lab15/js/lab.js
@@ -0,0 +1,18 @@
+/*
+* Author: Jonathan Madriaga
+* Created: 4 March
+* License: Public Domain
+*/
+
+
+var thisButton = $("#activate");
+
+$(thisButton).click(function(){
+  $.ajax({
+    url: "numbersapi.com/87";
+    type: "GET";
+    dataType: json;
+  });
+  var divider = document.getElementById("output");
+  divider.innerText(data);
+});
