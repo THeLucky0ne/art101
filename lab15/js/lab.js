ab194fde7abeef161577a9808651639a70b0e022
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-03-03 14:48:52 -0800

    made json a string

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -11,7 +11,7 @@ $(thisButton).click(function(){
   $.ajax({
     url: "numbersapi.com/87",
     type: "GET",
-    dataType: json,
+    dataType: "json",
   });
   var divider = document.getElementById("output");
   divider.innerText(data);
