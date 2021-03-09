1dde4d1e8a2a487d7cd5b3e636755451e5345ef5
THeLucky0ne <77127329+THeLucky0ne@users.noreply.github.com>
2021-03-08 17:05:14 -0800

    added stuff to html

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,3 +3,21 @@
 * Created: 8 March
 * License: Public Domain
 */
+
+
+function theAjax() {
+   $.ajax({
+    url: "https://xkcd.com/476/info.0.json",
+    data: {},
+    type: "GET",
+    dataType: "json",
+    success: theSuccess,
+    error: theFailure,
+
+  });
+}
+
+function theSuccess(comicObj){
+  console.log(comicObj);
+  $("#output").html(comicObj.title);
+}
